8b391ce0e11c11e461c2348475d3c0268a267486
Thuan Nguyen <thanhthuan.ng92@gmail.com>
2022-07-22 13:05:37 +0700

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Thuan, Nguyen Thanh on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
